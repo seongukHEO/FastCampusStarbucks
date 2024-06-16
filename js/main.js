ec2d1e312784596f442e79abec24ddf69014ba02
seongukHEO <heoseonguk3@gmail.com>
2024-06-16 23:36:04 +0900

    feat : 7차 수업

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const searchEl = document.querySelector('.search')
+const searchInput = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function(){
+    //로직 입력
+    searchInput.focus()
+})
+
+searchInput.addEventListener('focus', function(){
+    searchEl.classList.add('focused')
+    searchInput.setAttribute('placeholder', '통합검색')
+})
+
+searchInput.addEventListener('blur', function(){
+    searchEl.classList.remove('focused')
+    searchInput.setAttribute('placeholder', '')
+})
+
