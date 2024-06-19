e54450e41a787d87fbfd7974476d3d3c7e1cdecb
seongukHEO <heoseonguk3@gmail.com>
2024-06-19 19:58:34 +0900

    feat : 30차 수업

--- js/main.js
+++ js/main.js
@@ -112,7 +112,9 @@ spyEls.forEach(function(spyEl){
     new ScrollMagic.Scene({
         triggerElement: spyEl, 
         triggerHook: 0.8
-    }).setClassToggle().addTo()
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller())
 })
 
 
