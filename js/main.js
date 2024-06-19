820a1d066f768b848d3af32dbb3b56e3d95ad596
seongukHEO <heoseonguk3@gmail.com>
2024-06-19 19:29:09 +0900

    feat: 29차 수업

--- js/main.js
+++ js/main.js
@@ -107,6 +107,13 @@ function floationObject(selector){
 
 floationObject('.floating')
 
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl){
+    new ScrollMagic.Scene({
+        triggerElement: spyEl, 
+        triggerHook: 0.8
+    }).setClassToggle().addTo()
+})
 
 
 
