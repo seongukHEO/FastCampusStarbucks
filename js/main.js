ff7b160a7872c8c4f295a691578be84e01f308eb
seongukHEO <heoseonguk3@gmail.com>
2024-06-19 22:44:46 +0900

    feat : 프로젝트 마무리

--- js/main.js
+++ js/main.js
@@ -29,16 +29,37 @@ window.addEventListener('scroll' , _.throttle(
                 opacity: 0,
                 display : 'none' 
             })
+            //버튼 보이기
+            gsap.to('#to-top' , 0.2, {
+                x: 0,
+                
+            })
+
         }else{
             //배찌 보이기
             gsap.to(badgeEl, 0.6, {
                 opacity: 1,
                 display : 'block'
             })
+
+            //버튼 숨기기
+            gsap.to('#to-top' , 0.2, {
+                x: 100,
+
+            })
             
         }
     }, 300
 ))
+
+const toTopEl = document.querySelector('#to-top')
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, 0.7, {
+        scrollTo: 0
+    })
+})
+
+
 
 const fadeEls = document.querySelectorAll('.visual .fade-in')
 fadeEls.forEach(function(fadeEl, index){
@@ -139,6 +160,9 @@ spyEls.forEach(function(spyEl){
 const thisYear = document.querySelector('.this-year')
 //요소가 가지고 있는 text
 thisYear.textContent = new Date().getFullYear()
+
+
+
 
 
 
